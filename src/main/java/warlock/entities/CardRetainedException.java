f56b49dbfc2a0efb7c3bc4ca2cd739fe1bae699d
Michal Jastak <mjastak@gmail.com>
2011-08-15 08:09:38 +0200

    initial import

---
+++ src/main/java/warlock/entities/CardRetainedException.java
@@ -0,0 +1,17 @@
+package warlock.entities;
+
+/**
+ * Thrown when invalid card is entered into ATM.
+ * 
+ * @author warlock
+ */
+public class CardRetainedException extends RuntimeException {
+
+    /**
+     * Constructs new instance.
+     */
+    public CardRetainedException() {
+        super();
+    }
+
+}
---
+++ src/main/java/warlock/entities/InsufficientFundsException.java
@@ -0,0 +1,17 @@
+package warlock.entities;
+
+/**
+ * Thrown when the account has insufficient funds for the requested operation.
+ * 
+ * @author warlock
+ */
+public class InsufficientFundsException extends RuntimeException {
+
+    /**
+     * Construct new instance.
+     */
+    public InsufficientFundsException() {
+        super();
+    }
+
+}
